449d3ed20a49f674a7d158dec4002ebf383cf376
unknown <bamontelucas@gmail.com>
2018-11-30 20:16:04 -0200

    Module #2 finished

--- sick-fits/frontend/components/Meta.js
+++ sick-fits/frontend/components/Meta.js
@@ -5,7 +5,7 @@ const Meta = () => {
     return (
         <Head>
             <meta name="viewport" content="width=device-width" />
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <link rel="shortcut icon" href="/static/favicon.png" />
             <link rel="stylesheet" href="/static/nprogress.css" />
             <title>Sick Fits!</title>
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,17 +1,33 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => {
     return (
-        <div>
+        <NavStyles>
             <Link href="/">
                 <a>Home</a>
             </Link>
             <Link href="/sell">
                 <a>Sell</a>
             </Link>
-        </div>
+            <Link href="/items">
+                <a>items</a>
+            </Link>
+            <Link href="/sell">
+                <a>sell</a>
+            </Link>
+            <Link href="/signup">
+                <a>signup</a>
+            </Link>
+            <Link href="/orders">
+                <a>orders</a>
+            </Link>
+            <Link href="/me">
+                <a>Account</a>
+            </Link>
+        </NavStyles>
     );
 };
 
