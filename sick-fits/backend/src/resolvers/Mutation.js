842bb66add92aab05ffdc749059e9b5ddff1db0e
unknown <bamontelucas@gmail.com>
2018-12-30 16:49:22 -0200

    video #14

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        const input = { data: { ...args } };
+        const item = await ctx.db.mutation.createItem(input, info);
+
+        return item;
+    }
+};
 
 module.exports = Mutations;
